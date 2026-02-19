9d241d3e8f2b5534281038800e723f6c38109549
Bluperman949 <christiandoolittle@hotmail.com>
2026-02-19 11:29:55 -0800

    create create_db.sql

---
+++ src/create_db.sql
@@ -0,0 +1,11 @@
+/******************************************************************************
+* create_db.sql
+*
+* Authors: Christian Doolittle
+* Description:
+*   This script creates the database for our coffee bean company.
+******************************************************************************/
+
+DROP DATABASE IF EXISTS coffee;
+CREATE DATABASE coffee;
+USE coffee;
