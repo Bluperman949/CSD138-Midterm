3a7b137f1cf5963a7fe1fad1b223727cd0ba73dd
Bluperman949 <christiandoolittle@hotmail.com>
2026-02-19 11:34:37 -0800

    prep for generating fake data

---
+++ src/generator.py
@@ -0,0 +1,17 @@
+#
+#  generator.py
+#  Christian Doolittle
+#  19-Feb-2026
+#
+
+"""
+Generates fake data for our coffee company database.
+"""
+
+
+
+
+def main():
+  pass
+
+if (__name__ == '__main__'): main()
